d41fc74cf4376f103ee21e863b12d27ae34775ed
Luke Davids <lukedavids@gmail.com>
2025-03-19 15:49:54 -0500

    Added styling for combo special

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,8 @@ import { createCookies } from "./cookies.js";
 import { database } from "./database.js"
 import { salesOnClick } from "./functions.js";
 
-document.getElementById('comboButton').addEventListener('click', salesOnClick);
+// document.getElementById('comboButton').addEventListener('click', salesOnClick);
 
-
-
-const cart = []
 
 const cookieHTML = createCookies(database)
 const cookieContainer = document.querySelector('#container__cookies')
@@ -18,12 +15,12 @@ const iceCreamHTML = createIceCream(database)
 const iceCreamContainer = document.querySelector('#container__icecream')
 iceCreamContainer.innerHTML = iceCreamHTML;
 
-const button = document.querySelector("btn")
-button.addEventListener()
+
 document.getElementById('comboButton').addEventListener('click', () => {
     const randomSaleHTML = salesOnClick();
-    document.getElementById('random-sale-container').innerHTML = randomSaleHTML;
+    document.getElementById('container__special').innerHTML = randomSaleHTML;
 })
 
 
 
+    
